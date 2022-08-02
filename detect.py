2979d60d3e6a24f4153319e889691f02ae7dafc5
Krasjet <1291170799@qq.com>
2022-08-03 02:14:58 +0800

    fix plot

--- detect.py
+++ detect.py
@@ -11,7 +11,7 @@ from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
+from utils.plots import plot_only_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
@@ -110,7 +110,8 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                        plot_only_box(xyxy, im0, color=colors[int(cls)], line_thickness=3)
+                        # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
