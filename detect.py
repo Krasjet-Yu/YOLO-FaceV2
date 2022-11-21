6b52b8b046db5d20f1fc372f4fcd31bc99c57418
ZipingYu <krasjet.ziping@gmail.com>
2022-11-21 12:19:22 +0800

    fix stride

--- detect.py
+++ detect.py
@@ -32,7 +32,7 @@ def detect(save_img=False):
 
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
+    stride = int(model.stride.max())*2  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
     if half:
         model.half()  # to FP16
